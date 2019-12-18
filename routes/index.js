b056cca101c0cccbd37e73bcbf141caa432350df
unknown <as463431@gmail.com>
2019-12-19 03:40:39 +0800

    change to PUG

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('First_page');
 });
 
+/*GET Sign in page*/
+router.get('/signin', function(req, res, next) {
+  res.render('Sign_in');
+});
+
+/*GET Sign up page*/
+router.get('/signup', function(req, res, next) {
+  res.render('Sign_up');
+});
 module.exports = router;
