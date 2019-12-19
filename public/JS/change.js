c6a6cf11532dc0aa8f4bf9d8995af8be5dd54059
unknown <as463431@gmail.com>
2019-12-20 07:18:48 +0800

    JSS

--- public/JS/change.js
+++ public/JS/change.js
@@ -2,5 +2,47 @@
 function change()
 {
     console.log("change");
+    var token=$('input[name=csrfimiddlewaretoken').val();
+    var oldpassword= $("#oldpassword").val();
+    var newpassword=$("#newpassword").val();
+    var newNickName=$("#newnickname").val();
+    var alert=document.querySelector('#alert');
+
+    console.log(oldpassword);
+    console.log(newpassword);
+    console.log(newNickName);
+
+    var obj = {
+        csrfmiddlewaretoken: token,
+        old_password:oldpassword,
+        password:newpassword,
+        nick_name:newNickName
+    };
+
+    var json = JSON.stringify(obj);
+
+
+   $.ajax({
+        type:'POST',
+        url:"/api/v1/user/update" ,
+        data: obj ,
+        dataType: 'json'
+    }).done(function(data){
+        console.log(data);
+        var stutas=data.ok;
+        console.log(stutas);
+        if(stutas==false)
+        {
+            alert.style.display="block";
+            alert.textContent="The old password mismatch."
+        }
+            
+        else
+        {
+            alert.style.display="none";
+            window.location.assign('/user_info');
+        }
+           
+    }).fail(function(err){console.log(err)})
 }
 document.querySelector('.user_change').addEventListener('click',change );
